daa7b1d55f445a83556184ef7cfa627db65bc60b
himekuri <himekuri9@gmail.com>
2021-01-28 11:15:24 +0000

    add favorites-fixed

--- resources/views/favorites/favorite_button.blade.php
+++ resources/views/favorites/favorite_button.blade.php
@@ -1,5 +1,4 @@
-@if (Auth::id() != $micropost->id)
-    @if (Auth::user()->is_favorited($micropost->id))
+@if (Auth::user()->is_favorited($micropost->id))
         {{-- お気に入り解除ボタンのフォーム --}}
         {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
             {!! Form::submit('UnFavorite', ['class' => "btn btn-danger btn-sm"]) !!}
@@ -10,4 +9,3 @@
             {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-sm"]) !!}
         {!! Form::close() !!}
     @endif
-@endif

836e2e4bca48caaa330725d6c73b45c177a7be12
himekuri <himekuri9@gmail.com>
2021-01-28 03:04:07 +0000

    timeline

--- app/User.php
+++ app/User.php
@@ -44,13 +44,6 @@ class User extends Authenticatable
     {
         return $this->hasMany(Micropost::class);
     }
-    /**
-     * このユーザに関係するモデルの件数をロードする。
-     */
-    public function loadRelationshipCounts()
-    {
-        $this->loadCount('microposts');
-    }
     
     /**
      * このユーザがフォロー中のユーザ。（ Userモデルとの関係を定義）
@@ -132,6 +125,19 @@ class User extends Authenticatable
     public function loadRelationshipCounts()
     {
         $this->loadCount(['microposts', 'followings', 'followers']);
+    }
+    
+    /**
+     * このユーザとフォロー中ユーザの投稿に絞り込む。
+     */
+    public function feed_microposts()
+    {
+        // このユーザがフォロー中のユーザのidを取得して配列にする
+        $userIds = $this->followings()->pluck('users.id')->toArray();
+        // このユーザのidもその配列に追加
+        $userIds[] = $this->id;
+        // それらのユーザが所有する投稿に絞り込む
+        return Micropost::whereIn('user_id', $userIds);
     }
 }
 
